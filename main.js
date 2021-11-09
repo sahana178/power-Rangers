249559569224755c7a4d0850b868f8f6e80c5f93
sahana178 <85974914+sahana178@users.noreply.github.com>
2021-11-08 19:59:54 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 // Create canvas variable
-<var canvas = new fabric.Canvas('myCanvas');
+ canvas = new fabric.Canvas('myCanvas');
  block_y=1;
  block_x=1;
 
@@ -11,17 +11,18 @@ var block_image_object= "";
 
 function new_image(get_image)
 {
-    fabric.Image.fromURL(get_image, function(Img)){
+    fabric.Image.fromURL(get_image, function(Img){
 	block_image_object = Img;
 
 	block_image_object.scaleToWidth(block_image_width);
-	block_image_object.scaleToWidth(block_image_height);
+	block_image_object.scaleToHeight(block_image_height);
 	block_image_object.set({
 		top:block_y,
 		left:block_x
 	});
 	canvas.add(block_image_object);
-};
+});
+}
 
 window.addEventListener("keydown", my_keydown);
 
@@ -69,4 +70,3 @@ console.log(keyPressed);
 	}
 	
 }
-
